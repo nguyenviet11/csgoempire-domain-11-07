cda9735b1e73826b9b1563053cd112489141d0d4
vietnv <nguyenvanvietbn1998@gmail.com>
2025-07-18 18:18:42 +0700

    xxx

--- src/app/review-csgo/page.js
+++ src/app/review-csgo/page.js
@@ -5,13 +5,13 @@ import image from "../../../public/image.png";
 import Image from "next/image";
 
 export default function Home() {
-  const handleClick = () => {
-      window.location.href = "/empire-loading"; 
-  }
+  // const handleClick = () => {
+  //     window.location.href = "/empire-loading"; 
+  // }
 
   return (
     <div
-      onClick={handleClick}
+      // onClick={handleClick}
     >
       <main className={styles.main}>
         <h3 className={styles.text}>
